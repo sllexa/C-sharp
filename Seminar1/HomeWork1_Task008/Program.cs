41ba411cf1e70baaaf2893815fdf06a0bdb4eef0
Aleksei Slivko <sllexa@mail.ru>
2022-08-10 15:46:17 +0300

    Changing structure

---
+++ Seminar1/HomeWork1_Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        string mes = (i == N || i == N - 1) ?  i + "" : i + ", ";
+        Console.Write(mes);
+    }
+}
