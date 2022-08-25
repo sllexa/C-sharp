9653177536d96092cd880708d6a6be52bd0a0c47
Aleksei Slivko <sllexa@mail.ru>
2022-08-25 12:35:55 +0300

    Added homework for seminar 6

---
+++ Seminar6/HomeWork6_Task041/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2-> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountPlus(string[] arrStr)
+{
+    int count = 0;
+    for (int i = 0; i < arrStr.Length; i++)
+    {
+        if (Convert.ToInt32(arrStr[i]) > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите числа через пробел: ");
+string[] str = Console.ReadLine().Split(" ");
+Console.WriteLine($"Всего чисел больше 0: {CountPlus(str)}");
---
+++ Seminar6/HomeWork6_TaskDop/Program.cs
@@ -0,0 +1,37 @@
+﻿// Доп. задча: Задайте двумерный массив со случайными числами от -10 до 10. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.)
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1 + 9 + 2 = 12
+
+int[,] FillArray(int len0, int len1, int min, int max)
+{
+    int[,] array = new int[len0, len1];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int Sum(int[,] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j) sum += arr[i, j];
+        }
+    }
+    return sum;
+}
+
+int[,] arrNumbers = FillArray(4, 4, -10, 10);
+Console.WriteLine($"Сумма элементов главной диагонали: {Sum(arrNumbers)}");
