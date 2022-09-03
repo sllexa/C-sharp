2de347a39d88de1d7192d0074ee9e20959ca7661
Aleksei Slivko <sllexa@mail.ru>
2022-09-03 16:37:02 +0300

    Added homework for seminar 9

---
+++ Seminar9/HomeWork9_Task066/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15-> 120
+// M = 4; N = 8. -> 30
+
+int M = 1, N = 15;
+int sum = 0;
+SumNumbers(M, N);
+Console.WriteLine($"{sum}");
+
+void SumNumbers(int min, int max)
+{
+    sum += min++;
+    if (min <= max)
+    {
+        SumNumbers(min, max);
+    }
+}
---
+++ Seminar9/HomeWork9_Task068/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3->A(m, n) = 9
+
+int Ackkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackkerman(m - 1, 1);
+    else return Ackkerman(m - 1, Ackkerman(m, n - 1));
+}
+
+int m = 2, n = 3;
+
+Console.WriteLine("Функция Аккермана равна: {0}", Ackkerman(m, n));
